0510eb87f19e43ae5dddd64bba28f17c503f6d31
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-06-18 20:49:28 -0400

    Actualizando estructura del controlador

--- controllers/mailing.controller.js
+++ controllers/mailing.controller.js
@@ -1,25 +1,26 @@
 const { transporter } = require("../config/nodemailer.js");
 const dotenv = require("dotenv").config();
 
-const enviarEmailNuevo = (subject, content) => {
+const enviarEmailNuevo = async (req, res) => {
+  const { nombre, email, mensaje } = req.body;
   /* Enviar un mail a través del formulario */
   let mailOptions = {
     from: "Formulario Portafolio",
     to: process.env.CONTACT_EMAIL,
-    subject,
-    html: content,
+    subject: `Formulario Contacto: Remitente ${nombre} - Correo: ${email}`,
+    html: mensaje,
   };
 
   transporter.sendMail(mailOptions, (err, data) => {
     if (err) {
       console.log("Error, el mail no se ha enviado: " + err);
-      return {
+      res.status(500).json({
         msg: "Error, el mail no se ha enviado: ",
         error: err.message,
-      };
+      });
     } else {
       console.log("Email enviado correctamente: ", data);
-      return { msg: "Email enviado correctamente", error: err.message };
+      res.status(200).json({ msg: "Email enviado correctamente", data });
     }
   });
 };
--- routes/form.routes.js
+++ routes/form.routes.js
@@ -1,24 +1,12 @@
 const { Router } = require("express");
 const router = Router();
-const { enviarEmailNuevo } = require("../controllers/mailingController.js");
+const { enviarEmailNuevo } = require("../controllers/mailing.controller.js");
 const {
   createReview,
   readReviews,
 } = require("../controllers/tutorias.controller.js");
 
-router.post("/sendEmail", (req, res) => {
-  try {
-    const { nombre, email, mensaje } = req.body;
-    enviarEmailNuevo(
-      `Formulario Contacto: Remitente ${nombre} - Correo: ${email}`,
-      mensaje
-    );
-    res.status(200).json({ msg: "Email enviado correctamente" });
-  } catch (error) {
-    const err = new Error("Error al Enviar Correo");
-    res.status(500).json({ msg: err.message });
-  }
-});
+router.post("/sendEmail", enviarEmailNuevo);
 
 router.post("/sendReview", createReview);
 
